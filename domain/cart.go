66f75eeebecb513b5b90100874717e99842cdfb8
smith-30 <kohei.misu@techtouch.co.jp>
2021-07-04 15:30:14 +0900

    mod: cart

---
+++ domain/cart.go
@@ -0,0 +1,64 @@
+package domain
+
+type Item struct {
+	Price float64
+}
+
+type EmptyCart struct {
+}
+
+func (a *EmptyCart) Items() []Item {
+	return nil
+}
+
+type ActiveCart struct {
+	UnpaidItems []Item
+}
+
+func (a *ActiveCart) Items() []Item {
+	return nil
+}
+
+type PaidCart struct {
+	PaidItems []Item
+	Payment   float64
+}
+
+func (a *PaidCart) Items() []Item {
+	return nil
+}
+
+type ShoppingCart interface {
+	Items() []Item
+}
+
+func addItem(cart ShoppingCart, item Item) ShoppingCart {
+	switch v := cart.(type) {
+	case *EmptyCart:
+		return &ActiveCart{
+			UnpaidItems: []Item{item},
+		}
+	case *ActiveCart:
+		re := &ActiveCart{
+			UnpaidItems: []Item{item},
+		}
+		re.UnpaidItems = append(re.UnpaidItems, v.UnpaidItems...)
+		return re
+	case *PaidCart:
+	}
+	return cart
+}
+
+func makePayment(cart ShoppingCart, payment float64) ShoppingCart {
+	switch v := cart.(type) {
+	case *EmptyCart:
+	case *ActiveCart:
+		re := &PaidCart{
+			Payment: payment,
+		}
+		re.PaidItems = append(re.PaidItems, v.UnpaidItems...)
+		return re
+	case *PaidCart:
+	}
+	return cart
+}
